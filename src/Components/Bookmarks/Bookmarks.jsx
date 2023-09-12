52df01783aaff57f23cc63d635d82a81951b709e
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-12 09:21:01 +0600

    handle bookmark

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,7 +1,7 @@
 const Bookmarks = () => {
     return (
         <div className="md: w-1/3">
-            <h2>Bookmarks</h2>
+            <h2 className="text-2xl"> Bookmarked Blogs:</h2>
         </div>
     );
 };
